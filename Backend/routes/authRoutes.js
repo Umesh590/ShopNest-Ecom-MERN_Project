2159676a6c7b9ed33445ebdc9576c6d347031b3e
student-Umeshkumar <umeshjha7890@gmail.com>
2026-06-09 17:17:39 +0530

    Fix auth middleware filename

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { registerUser, verifyOTP, loginUser, getUsers } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authmiddleware');
 const { admin } = require('../middleware/adminMiddleware');
 const router = express.Router();
 
